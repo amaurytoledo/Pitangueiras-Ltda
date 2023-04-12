a6bf3b6715b76ca250a109a5c75fdfd45bbefbe8
Amaury Toledo <69945431+amaurytoledo@users.noreply.github.com>
2023-04-12 13:36:45 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,40 +1,33 @@
 <?php
 
-$login = $_POST['login'];
-$senha = MD5($_POST['senha']);
+$chamado = $_POST['chamado'];
+$descricao = $_POST['descricao'];
 $connect = mysqli_connect('localhost','root','');
-$db = mysqli_select_db('db_pitangueira');
-$query_select = "SELECT login FROM tb_func WHERE login = '$login'";
-$select = mysqli_query($query_select,$connect);
+$db = mysqli_select_db($connect, 'db_pitangueira');
+$query_select = "SELECT chamado FROM tb_atividade WHERE chamado = '$chamado'";
+$select = mysqli_query($connect, $query_select);
 $array = mysqli_fetch_array($select);
-$logarray = $array['login'];
+$logarray = $array['chamado'];
 
-  if($login == "" || $login == null){
-    echo"<script language='javascript' type='text/javascript'>
-    alert('O campo login deve ser preenchido');window.location.href='
-    cadastro.html';</script>";
-
-    }else{
-      if($logarray == $login){
-
-        echo"<script language='javascript' type='text/javascript'>
-        alert('Esse login já existe');window.location.href='
-        cadastro.html';</script>";
+if($chamado == "" || $chamado == null){
+    echo "<script language='javascript' type='text/javascript'>
+    alert('O campo chamado deve ser preenchido');window.location.href='index.html';</script>";
+}else{
+    if($logarray == $chamado){
+        echo "<script language='javascript' type='text/javascript'>
+        alert('Esse chamado já existe');window.location.href='index.html';</script>";
         die();
-
-      }else{
-        $query = "INSERT INTO tb_func (login,senha) VALUES ('$login','$senha')";
-        $insert = mysqli_query($query,$connect);
+    }else{
+        $query = "INSERT INTO tb_atividade (chamado,descricao) VALUES ('$chamado','$descricao')";
+        $insert = mysqli_query($connect, $query);
 
         if($insert){
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Usuário cadastrado com sucesso!');window.location.
-          href='login.html'</script>";
+            echo "<script language='javascript' type='text/javascript'>
+            alert('Chamado cadastrado com sucesso!');window.location.href='index.html'</script>";
         }else{
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Não foi possível cadastrar esse usuário');window.location
-          .href='cadastro.html'</script>";
+            echo "<script language='javascript' type='text/javascript'>
+            alert('Não foi possível cadastrar esse chamado');window.location.href='index.html'</script>";
         }
-      }
     }
+}
 ?>
