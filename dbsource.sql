87345950f7fa61284cd754ed77891af71b6df33c
Amaury Toledo <69945431+amaurytoledo@users.noreply.github.com>
2023-04-12 13:37:20 -0300

    Update dbsource.sql

--- dbsource.sql
+++ dbsource.sql
@@ -1,20 +1,21 @@
-create database db_pitangueira;
+CREATE DATABASE db_pitangueira;
 
-use db_pitangueira;
+USE db_pitangueira;
+
 CREATE TABLE tb_func(
     id_func INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(50),
     senha VARCHAR(20)
 );
 
-
 CREATE TABLE tb_atividade(
     id_atv INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     chamado VARCHAR(50),
-    descricao VARCHAR(20)
+    descricao VARCHAR(255)
 );
 
+DROP TABLE IF EXISTS tb_func;
 
-drop database db_pitangueira;
+DROP TABLE IF EXISTS tb_atividade;
 
-drop table tb_func;
+DROP DATABASE IF EXISTS db_pitangueira;
