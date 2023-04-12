d5378dc9cc45c4bf64b59d71478d77d9dfde9f07
Amaury Toledo <69945431+amaurytoledo@users.noreply.github.com>
2023-04-12 13:49:28 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,33 +1,38 @@
 <?php
+$login = $_POST['login'];
+$senha = md5($_POST['senha']);
+$funcao = $_POST['funcao'];
 
-$chamado = $_POST['chamado'];
-$descricao = $_POST['descricao'];
-$connect = mysqli_connect('localhost','root','');
-$db = mysqli_select_db($connect, 'db_pitangueira');
-$query_select = "SELECT chamado FROM tb_atividade WHERE chamado = '$chamado'";
+$connect = mysqli_connect('localhost', 'root', '', 'db_pitangueira');
+
+$query_select = "SELECT login FROM tb_func WHERE login = '$login'";
 $select = mysqli_query($connect, $query_select);
 $array = mysqli_fetch_array($select);
-$logarray = $array['chamado'];
+$logarray = $array['login'];
 
-if($chamado == "" || $chamado == null){
-    echo "<script language='javascript' type='text/javascript'>
-    alert('O campo chamado deve ser preenchido');window.location.href='index.html';</script>";
-}else{
-    if($logarray == $chamado){
-        echo "<script language='javascript' type='text/javascript'>
-        alert('Esse chamado já existe');window.location.href='index.html';</script>";
-        die();
-    }else{
-        $query = "INSERT INTO tb_atividade (chamado,descricao) VALUES ('$chamado','$descricao')";
-        $insert = mysqli_query($connect, $query);
+if ($login == "" || $login == null) {
+	echo "<script language='javascript' type='text/javascript'>
+	alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+}
+else {
+	if ($logarray == $login) {
+		echo "<script language='javascript' type='text/javascript'>
+		alert('Esse login já existe');window.location.href='cadastro.html';</script>";
+		die();
+	}
+	else {
+		$query = "INSERT INTO tb_func (login,senha,funcao) VALUES ('$login','$senha','$funcao')";
+		$insert = mysqli_query($connect, $query);
 
-        if($insert){
-            echo "<script language='javascript' type='text/javascript'>
-            alert('Chamado cadastrado com sucesso!');window.location.href='index.html'</script>";
-        }else{
-            echo "<script language='javascript' type='text/javascript'>
-            alert('Não foi possível cadastrar esse chamado');window.location.href='index.html'</script>";
-        }
-    }
+		if ($insert) {
+			echo "<script language='javascript' type='text/javascript'>
+			alert('Usuário cadastrado com sucesso!');window.location.href='index.html'</script>";
+		}
+		else {
+			echo "<script language='javascript' type='text/javascript'>
+			alert('Não foi possível cadastrar esse usuário');window.location.href='cadastro.html'</script>";
+		}
+	}
 }
 ?>
+
